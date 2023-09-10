a934cac729a4d554ff5d1244610ecf91153271a3
siya2910 <143694322+siya2910@users.noreply.github.com>
2023-09-10 18:40:12 +0530

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std ;
+
+int main(){
+
+     int num1 ,num2;
+    cout<<"enter the value of num1 : \n"; /*<< is called the insertion operator*/
+    cin>>num1;/*>>is called an extraction operator*/ 
+
+    cout<<"enter the value of num2 : \n";/*<<is called the insertion operator*/ 
+    cin>>num2;/*>>is called an extraction operator*/ 
+
+    cout<<"the sum is :"<<num1+num2 ;
+
+return 0;
+
+}
+        
+
+
+

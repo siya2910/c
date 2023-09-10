c74e9d18c2b9f300617fa232bcb725ef1b1d3d20
siya2910 <143694322+siya2910@users.noreply.github.com>
2023-09-10 18:39:49 +0530

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;  
+int glo=6;
+void sum (){
+    int a;
+    cout<< glo;
+}
+int main (){
+    int a= 29; 
+    int b= 13;
+    float pie =3.15;
+    char c='d';
+    bool is_true = true;
+sum();
+    cout << glo<<is_true;
+    cout<<"this is my 4 th class .\n here the value of a is "<<a<<".\nthe value of b is "<< b  ;
+    cout<<"\nthe value of pie is :"<<pie; 
+    cout<<"\n The value of c is :"<<c;
+    return 0;
+}
+  

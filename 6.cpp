2eaeaeb5cf595c9a5263de96f6e7e0191e228932
Siya Mandal <143694322+siya2910@users.noreply.github.com>
2023-09-12 19:31:06 +0530

    Add files via upload

---
+++ 6.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+ // int a=29;
+ //cout<<"The value of a was:" <<a<,endl;
+ //a=35;
+ //cout<<"The value of a is:" <<a<,endl;
+
+ //constants in c++.....//
+//const int a=35;
+//cout<<"the value of a was:"<<a;
+//a=37;//you will find an error because constant is used
+//cout<<"the value of a is:"<<a;
+
+//maipulators in c++//
+//int a=5 ,b=355, c=455;
+//cout<<"THE value of a without setw is:"<<a<<endl;
+//cout<<"THE value of b without setw is:"<<b<<endl;
+//cout<<"THE value of c without setw is:"<<c<<endl;
+
+//cout<<"THE value of a with setw is:"<<setw(6)<<a<<endl;
+//cout<<"THE value of b with setw is:"<<setw(6)<<b<<endl;
+//cout<<"THE value of c with setw is:"<<setw(6)<<c<<endl;
+
+//operator precedence in c++//
+int a=3,b=5;
+int  c=(((a*b)+45)-92);
+cout<<c;
+    return 0;
+}

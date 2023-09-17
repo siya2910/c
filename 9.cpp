2299b4016e17cff9a861790cde7faac60ccfbba2
Siya Mandal <143694322+siya2910@users.noreply.github.com>
2023-09-17 18:59:41 +0530

    Add files via upload

---
+++ 9.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+ //int main(){
+   //    for ( int i = 0; i < 40; i++)
+   // {
+   //     cout<<i<<endl;
+   //     if (i==2)
+   //     {
+   //         break;
+   //     }
+   // }
+    
+   // return 0;
+    int main(){
+       for ( int i = 0; i < 40; i++)
+    {   cout<<i<<endl;
+        if (i==2)
+        {
+            continue;
+        }
+    }
+        
+    return 0;
+}
+
